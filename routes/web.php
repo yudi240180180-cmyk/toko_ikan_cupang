8e10592642eab54fb7a5df52544f59ba1915f393
yudi240180180-cmyk <yudi.240180180@mhs.unimal.ac.id>
2026-04-20 23:44:22 +0700

    Fix: Kembali ke halaman welcome dan bereskan tampilan

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FishController;
 
 Route::get('/', function () {
-    return redirect('/ikan');
+    return view('welcome');
 });
 Route::resource('ikan', FishController::class);
